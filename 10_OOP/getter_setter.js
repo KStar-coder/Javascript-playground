c4686d64a1b509f248b87dd171d8f9c4738be182
KStar-coder <sharmakaustav04@gmail.com>
2024-02-27 20:23:54 +0530

    Initial commit

---
+++ 10_OOP/getter_setter.js
@@ -0,0 +1,54 @@
+class User {
+    constructor(email, password) {
+        this.email = email
+        this.password = password
+    }
+
+    // getters and setters 
+    // Here, we use a different name _password (you can choose any name) so that we do not confuse it with the constructor in password to avoid stack oveflow
+    get password() {
+        return this._password.toUpperCase()
+    }
+    set password(value) {
+        this._password = value
+    }
+
+    get email() {
+        return this._email
+    }
+    set email(value) {
+        this._email = value
+    }
+}
+
+const kaustav = new User("kaustav@kaustav.ai", "123")
+console.log(kaustav.password)
+
+// a 'function' is a function as well as an object 
+
+function Userfn(email, password) {
+    this._email = email;
+    this._password = password
+
+    // overriding a property in function
+    Object.defineProperty(this, 'email', {
+        get: function () {
+            return this._email = email
+        },
+        set: function (value) {
+            this.email = value
+        }
+    })
+
+    Object.defineProperty(this, 'password', {
+        get: function () {
+            return this._password = password
+        },
+        set: function (value) {
+            this._password = value
+        }
+    })
+}
+
+const chai = new Userfn("chai@chai.com", "chai")
+console.log(chai.email)
